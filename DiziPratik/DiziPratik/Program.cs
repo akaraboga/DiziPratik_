f3bd860a5534838a04601676e79ad89a9aab464b
Ahmet Karaboğa <130494000+akaraboga@users.noreply.github.com>
2024-11-11 22:36:48 +0300

    Add files via upload

---
+++ DiziPratik/DiziPratik/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiziPratik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] dizi = new int[10];//10 elemanlı diziyi tanımlıyoruz
+
+            Console.WriteLine("10 tane sayi giriniz");
+
+            for (int i = 0; i < dizi.Length; i++)//diziyi for ile alma
+            {
+                Console.WriteLine("{0}.Sayi giriniz", i + 1);
+                dizi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Girdiğiniz sayilar : ");
+
+            foreach (int sayilar in dizi)//Foreach ile yazdırma
+            {
+                Console.WriteLine(sayilar);
+            }
+
+
+
+            int[] dizi2 = new int[11];//11 elemanlı yani bir dizi tanımlıyoruz
+
+            for (int i = 0; i < dizi.Length; i++) 
+            {
+                dizi2[i] = dizi[i];//11 elemanlı diziye 10 elemanlı diziyi kopyalıyoruz
+            }
+
+
+   
+            
+
+            int Ydeger = 0;//Default olarak  kullanıcıdan gelecek olan değeri tanımlıyoruz
+
+        tekrar://Etiketin geldiği yer
+
+            Console.WriteLine("Yeni değer eklemek istiyor musunuz? E || H");//Eğer kullanıcı yeni değer girmek istiyorsanız
+            string cevap = Console.ReadLine().ToUpper();
+
+      
+
+           
+
+ 
+
+           
+            if (cevap == "E")//Cevap E ise 
+            {
+                Console.Write("Eklemek istediğiniz sayiyi giriniz : ");
+
+                Ydeger = Convert.ToInt32(Console.ReadLine());//İstedğimiz sayiyi giriyoruz
+
+                dizi2[dizi.Length] = Ydeger;//Girdiğimiz sayiyi dizi2 ye 11 haneli dizi2 ye atıyoruz
+
+                Array.Sort(dizi2);//Küçükten büyüğe doğru sıralar eğer bunu yapmazsak diziye girdiğimiz yeni değer hep en başa yazılır
+                Array.Reverse(dizi2);//Diziyi buyukten küçüğe sıralamak için
+
+                Console.WriteLine("Yeni sayilar :");
+
+                foreach (int sayi in dizi2)//Yeni hali
+                {
+                    Console.WriteLine(sayi);
+                }
+
+            }
+            else if (cevap == "H")//H ise aşağıdaki işlemleri yapıyoruz
+            {
+                Console.WriteLine("Yeni değer eklemediniz");
+                Console.WriteLine("Çıkmak için bir tuşa basınız");
+ 
+
+                foreach (int sayi in dizi)//Deger eklenmediği için 10 haneli dizi değerini bastırıyoruz
+                {
+                    Console.WriteLine(sayi);
+                }
+
+                Console.ReadKey();
+
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Yanlış cevap");
+               
+                goto tekrar;// E || H den baska cevap verdiyse goto etiketi ile başa donderiyoruz
+            }
+          
+
+           
+
+          
+           
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DiziPratik/DiziPratik/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DiziPratik")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DiziPratik")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("00215a35-cc4c-4517-b5a4-46a3f0b60ad2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
